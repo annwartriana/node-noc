4dffb835e6b57332183def95dc3872a39bd292cc
Annwar Triana <20819350+annwartriana@users.noreply.github.com>
2024-08-26 16:09:13 -0500

    Creación y uso de UseCase SendEmailLogs

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,7 +1,5 @@
 import nodemailer from "nodemailer";
 import { envs } from "../../config/plugins/envs.plugin";
-import { LogRepository } from "../../domain/repository/log.repository";
-import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 
 interface SendMailOptions {
   to: string | string[];
@@ -23,8 +21,6 @@ export class EmailService {
     },
   });
 
-  constructor(private readonly logRepository: LogRepository) {}
-
   async sendEmail(options: SendMailOptions): Promise<boolean> {
     const { to, subject, htmlBody, attachments = [] } = options;
     try {
@@ -35,23 +31,8 @@ export class EmailService {
         attachments,
       });
       console.log(sentInformation);
-      
-      const log = new LogEntity ({
-        level: LogSeverityLevel.low,
-        message: 'Email was sent',
-        origin: 'email.service.ts',
-      });
-      this.logRepository.saveLog(log);
-
       return true;
     } catch (error) {
-      console.log(error);
-      const log = new LogEntity ({
-        level: LogSeverityLevel.high,
-        message: 'Email was not sent',
-        origin: 'email.service.ts',
-      });
-      this.logRepository.saveLog(log);
       return false;
     }
   }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,21 +1,24 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasource/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
-import { EmailService } from './email/email.service';
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDatasource(),
+  new FileSystemDatasource()
 );
+const emailService = new EmailService();
 
 export class Server {
-
+  
   static start() {
     console.log("Server started");
 
-    const emailService = new EmailService(fileSystemLogRepository);
-    emailService.sendEmailWithFileSystemLogs('andrestorres8889@gmail.com');
-    
+    new SendEmailLogs(emailService, fileSystemLogRepository).execute(
+      "andrestorres8889@gmail.com"
+    );
+
     // CronService.createJob(
     //     '*/5 * * * * *',
     //     () => {
@@ -24,7 +27,7 @@ export class Server {
     //         fileSystemLogRepository,
     //         () => console.log( `${ url } is ok` ),
     //         ( error ) => console.log( error ),
-    //       ).execute( url );        
+    //       ).execute( url );
     //     }
     //   );
   }

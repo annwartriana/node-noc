100ac66fd8f487ed0527c2f48d769b0dd145a9c8
Annwar Triana <20819350+annwartriana@users.noreply.github.com>
2024-08-26 15:59:57 -0500

    Agregamos adjunto de logs y se log para envío de  mensajes

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasource/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from './email/email.service';
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource(),
@@ -12,16 +13,19 @@ export class Server {
   static start() {
     console.log("Server started");
 
-    CronService.createJob(
-        '*/5 * * * * *',
-        () => {
-          const url = 'https://localhost:3000';
-          new CheckService(
-            fileSystemLogRepository,
-            () => console.log( `${ url } is ok` ),
-            ( error ) => console.log( error ),
-          ).execute( url );        
-        }
-      );
+    const emailService = new EmailService(fileSystemLogRepository);
+    emailService.sendEmailWithFileSystemLogs('andrestorres8889@gmail.com');
+    
+    // CronService.createJob(
+    //     '*/5 * * * * *',
+    //     () => {
+    //       const url = 'https://google.com';
+    //       new CheckService(
+    //         fileSystemLogRepository,
+    //         () => console.log( `${ url } is ok` ),
+    //         ( error ) => console.log( error ),
+    //       ).execute( url );        
+    //     }
+    //   );
   }
 }
